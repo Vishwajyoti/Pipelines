945060c24275983ad3affd12614f7446949aa37b
vishwajyoti <vishwajyoti.pandey@publicissapient.com>
2019-09-14 23:29:14 +0530

    Changes in pipeline python script.

--- demo_kf_pipelines.py
+++ demo_kf_pipelines.py
@@ -5,9 +5,12 @@ Created on Tue Sep  3 14:58:04 2019
 """
 
 
+from kfp import compiler
 import kfp.dsl as dsl
-import kfp.compiler as compiler
+import kfp.gcp as gcp
+import kfp.notebook
 import sys
+import json
 
 class ObjectDict(dict):
   def __getattr__(self, name):
@@ -28,18 +31,14 @@ def ftreng_train_test_and_deploy(
         test_size=0.3,
         file_name='Boston.csv',
         target_var='target',
-        hyper_param={'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000],
-                     'max_features': ['auto', 'sqrt'],
-                     'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, None],
-                     'min_samples_split': [2, 5, 10],'min_samples_leaf': [1, 2, 4],
-                     'bootstrap': [True, False]},
+        hyper_param=json.dumps({'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000],'max_features': ['auto', 'sqrt'],'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, None],'min_samples_split': [2, 5, 10],'min_samples_leaf': [1, 2, 4],'bootstrap': [True, False]}),
         search_type=1,
         model_bucket_name='rf-vj-model',
         model_name='rf-vj',
         framework='scikit-learn',
         version_key_word='rf_model_'
         
-):
+        ):
 # Step 1: create training dataset using Apache Beam on Cloud Dataflow
     feature_eng = dsl.ContainerOp(
             name='feature_eng',
@@ -51,7 +50,7 @@ def ftreng_train_test_and_deploy(
                     '--t_size',test_size
                     ]
             #,file_outputs={'bucket': '/output.txt'}
-            )
+            ).apply(gcp.use_gcp_secret('user-gcp-sa'))
 
 # Step 2: Train the model and find best set of hyperparameter.
     train = dsl.ContainerOp(
@@ -65,7 +64,7 @@ def ftreng_train_test_and_deploy(
                     '--search_type',search_type
                     ]
             #,file_outputs={'jobname': '/output.txt'}
-            )
+            ).apply(gcp.use_gcp_secret('user-gcp-sa'))
     train.after(feature_eng)
 
 # Step 3: Train the model some more, but on the pipelines cluster itself
@@ -79,7 +78,7 @@ def ftreng_train_test_and_deploy(
                     '--target',target_var
                     ]
             #,file_outputs={'train': '/output.txt'}
-            )
+            ).apply(gcp.use_gcp_secret('user-gcp-sa'))
     test.after(train)
 
 # Step 4: Deploy the trained model to Cloud ML Engine
@@ -97,9 +96,9 @@ def ftreng_train_test_and_deploy(
                     model_name   
                     ]
 
-        )
+        ).apply(gcp.use_gcp_secret('user-gcp-sa'))
     deploy_cmle.after(test)
 
 if __name__ == '__main__':
-  filename = sys.argv[1]
-  compiler.Compiler().compile(ftreng_train_test_and_deploy,filename)
+    filename=sys.argv[1]
+    compiler.Compiler().compile(ftreng_train_test_and_deploy,filename)
